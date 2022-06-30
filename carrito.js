bed6296afce7ea5145562fa233e58f3710e92d61
Leonardo Aversa <aversaleonardo@hotmail.com>
2022-06-30 20:52:13 -0300

    secound commit

---
+++ carrito.js
@@ -0,0 +1,73 @@
+let carrito2;
+
+if (JSON.parse(localStorage.getItem('carrito'))){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+} else {
+    localStorage.setItem('carrito', JSON.stringify([]))
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+}
+
+const totalCarrito = () => {
+    return carrito.reduce((acc, pro) => acc + productos.precio * productos.cantidad, 0)
+}
+
+const body = document.getElementById('carrito')
+if(carrito.length == 0){
+    const texto = `
+    <div class='cartContainer'>
+        <h1 class='txtCarrito'>No hay productos en el carrito</h1>
+        <a class='btnVolver' href='index.html'>
+            <button>VOLVER</button>
+        </a>
+    </div>`;    
+    body.innerHTML += texto;
+} else {
+    const titulo = `
+        <div class='cartContainer'>
+            <h1 class='txtCarrito'>Carrito de compras</h1>
+        </div>`;
+        body.innerHTML += titulo;
+    const table = `
+        <div class='tableContainer'>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th class='txtTabla'>PRODUCTOS</th>
+                        <th class='txtTabla'>CANTIDAD</th>
+                        <th class='txtTabla'>PRECIO</th>
+                    </tr>
+                </thead>
+                <tbody id='tbody'>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th class='txtTotal'>Total:</th>
+                        <th id='total'>$${totalCarrito().toLocaleString()}</th>
+                    </tr>
+                </tfoot>
+            </table>
+            </div>
+            <div class='btn-container'>
+                <button class='btnTerminar'>TERMINAR COMPRA</button>
+            </div>`;
+
+            body.innerHTML += table
+            const tbody = document.getElementById("tbody")
+            for (let i = 0; i < carrito.length; i++) {
+                const element = carrito[i];
+                const {id, modelo, img, precio, cantidad} = element;
+                const cart = `
+                <tr id=${id}>
+                    <th><img class='trash' src='./assets/icon/cart.png' alt='foto de borrar'></th>
+                    <th class='detallesTabla'><img class='imgProdCart' src=${img} alt='foto del producto'><span class='nombreProd'>${nombre}</span></th>
+                    <th>${cantidad}</th>
+                    <th>$${(cantidad * precio).toLocaleString()}</th>
+                </tr>`
+                tbody.innerHTML += cart
+            }
+        }
+                
+   
---
+++ main.js
@@ -0,0 +1,153 @@
+const productos = [
+
+    {   
+
+    id: "1",
+    categoría: "asfalto", 
+    modelo: "Lite Racer Rebold", 
+    cantidad: 1, 
+    precio: 35000,
+    img: "./assets/img/adi3.jpg",
+    },
+
+    {    
+    id: "2",
+    categoría: "asfalto", 
+    modelo: "Adios Pro",  
+    cantidad: 1, 
+    precio: 38000,
+    img: "./assets/img/adi2.jpg",
+    },
+
+    {    
+    id: "3",
+    categoría: "asfalto", 
+    modelo: "Ultraboost Lego",  
+    cantidad: 1, 
+    precio: 42000,
+    img: "./assets/img/LEGO.jpg",
+    },
+    
+    { 
+    
+    id: "4",
+    categoría: "trial", 
+    modelo: "Trail Terrex Pro",  
+    cantidad: 1, 
+    precio: 45000,
+    img: "./assets/img/azules.jpg",
+    },
+
+    { 
+    
+    id: "5",
+    categoría: "asfalto", 
+    modelo: "Racer TR 21",  
+    cantidad: 1, 
+    precio: 32000,
+    img: "./assets/img/adi4.jpg",
+    },
+
+    {
+
+    id: "6",
+    categoría: "trial", 
+    modelo: "Terrex Two BOA",  
+    cantidad: 1, 
+    precio: 39000,
+    img: "./assets/img/adi5.jpg",
+    },
+
+    { 
+    
+    id: "7",
+    categoría: "asfalto", 
+    modelo: "Solarglide 5",  
+    cantidad: 1, 
+    precio: 41000,
+    img: "./assets/img/adi6.jpg",
+
+},
+    { 
+    
+    id: "8",
+    categoría: "asfalto", 
+    modelo: "Adizero Boston 10",  
+    cantidad: 1, 
+    precio: 40000,
+    img: "./assets/img/adi7.jpg",
+}
+
+
+]
+
+let carrito;
+
+if (JSON.parse(localStorage.getItem('carrito'))){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+} else {
+    localStorage.setItem('carrito', JSON.stringify([]))
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+}
+
+function desplegarproductos(){
+    for (let i = 0; i < productos.length; i++) {
+        const element = productos[i];
+        const{id, modelo, precio, img} = element
+        const card = `
+        <div class= 'card'>
+        <p>${modelo}</p>
+        <div>
+            <img class= 'imgProducto' src=${img} alt= ''/>
+        </div>
+        <div>
+            <p>$${precio.toLocaleString()}</p>
+        </div>
+        <div class="btn-container">
+            <button id= ${id} class= 'btnAgregar btn btn-primary'>Agregar</button>
+
+            </div>
+        </div>
+    `
+    const container = document.getElementById("container")
+    container.innerHTML += card
+    }
+}
+
+desplegarproductos()
+
+const btnAgregar = document.getElementsByClassName('btnAgregar')
+
+for (let i = 0; i < btnAgregar.length; i++) {
+    const element = btnAgregar[i];
+    element.addEventListener('click', agregarAlCarrito)
+
+    
+}
+
+
+
+function agregarAlCarrito(e) {
+const btn = e.target;
+const idBoton = btn.getAttribute('id')
+const prodEncontrado = productos.find(prod => prod.id == idBoton)
+const enCarrito = carrito.find(prod => prod.id == prodEncontrado.id)
+
+/* alert("agregaste") */
+
+console.log(enCarrito)
+
+if(!enCarrito) {
+    carrito.push({...prodEncontrado, cantidad: 1})
+} else {
+    let carritoFiltrado = carrito.filter(prod => prod.id != enCarrito.id)
+    carrito = [...carritoFiltrado, {...enCarrito, cantidad: enCarrito.cantidad + 1}]
+}
+
+console.log(carrito)
+localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+const contador = document.getElementById("cartCounter")
+contador.innerHTML = carrito.length
+
