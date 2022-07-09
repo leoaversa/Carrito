f4aa2c5ffb99ad78d963ca22348d3eab0431c948
Leonardo Aversa <aversaleonardo@hotmail.com>
2022-07-09 19:03:43 -0300

    octave commit

--- main.js
+++ main.js
@@ -117,26 +117,7 @@ const agregarAlCarrito = (prodId) => {
 }
 
 
-const eliminarDelCarrito = (prodId) => {
-    const item = carrito.find((prod) => prod.id === prodId)
-    const indice = carrito.indexOf(item)
-    carrito.splice(indice,1)
-    
 
-
-    Swal.fire({
-        position: 'top-center',
-        icon: 'success',
-        title: 'Producto eliminado',
-        showConfirmButton: false,
-        timer: 800
-      })
-    
-      
-    actualizarCarrito()
-    
-
-}
 
 //VACIAR CARRITO
 botonVaciar.onclick = () => {
@@ -148,6 +129,27 @@ botonVaciar.onclick = () => {
     actualizarCarrito()    
 
 }
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id == prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    actualizarCarrito()
+
+
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Producto eliminado',
+        showConfirmButton: false,
+        timer: 800
+      })   
+
+}
+
+
+
 
 
 
@@ -167,9 +169,12 @@ const actualizarCarrito = () => {
         <div><img src= "./assets/img/tarjetas.webp" alt=""></div>
         
         <button onclick= "eliminarDelCarrito(${prod.id})" type="button" class="btn btn-danger">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"></path>
-            </svg></button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 
+        0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 
+        0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 
+        4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"></path>
+        </svg></button>
         `
         
         
@@ -194,14 +199,12 @@ const actualizarCarrito = () => {
         //CONTADOR CARRITO DEL NAV
         contadorCarrito.innerText = (carrito.length)        
         
-    })
+     })
 }
         
 
         //FILTRAR 
-        const buscador = document.getElementById('buscador')
-        const filtrar = document.getElementById('filtrar')
-
+        
         function filtrarPorCat(){
 
             
@@ -232,7 +235,24 @@ const actualizarCarrito = () => {
                 showProducts.append(divfilter)              
                 
 
-                
+                const boton = document.getElementById(`agregar${filter.id}`)
+                boton.addEventListener('click', () => {
+                agregarAlCarrito(filter.id)
+
+            //SWEET ALERT
+            Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Agregaste al carrito',
+            text: (filter.modelo),
+            showConfirmButton: true,
+            timer: 2000
+            }) 
+        
+            })
+
+            
+
         
             })
 
@@ -243,32 +263,49 @@ const actualizarCarrito = () => {
             
 
             }
+        }   
+        
+        const buscador = document.querySelector('#buscador');
+        const filtrar = document.querySelector('#filtrar');
+        
+        const btnBuscar = () => {
             
-      
+            console.log(buscador.value);
+           }
 
-        }           
+          
+           const dale = () => {
+           e.preventDefault()
+
+        filtrar.addEventListener('click', btnBuscar)}
+
+        btnBuscar()
+
+        
+        
+        
 
         
     
-        buscador.onchange = () => {
+        /* buscador.onchange = () => {
+            
             contenedorProductos.innerHTML= ""          
             filtrarPorCat()
+            buscador.preventDefault();
             
-        }
+        } */
 
         
+        
 
-        filtrar.addEventListener ('click', () => {
-            contenedorProductos.innerHTML= ""
-            filtrarPorCat()
-        })
+        
 
         
     
 
         // TERMINAR COMPRA
 
-         function terminarCompra () {
+        /*  function terminarCompra () {
             class Usuario {
                 constructor(nombre, direccion, mail){
                     this.nombre = nombre,
@@ -286,15 +323,17 @@ const actualizarCarrito = () => {
          console.log(usuarioCompra)
          console.log(carrito)
 
-         let = document.getElementById("terminarPedido")
+         let terminarPedido= document.getElementById("terminarPedido")
 
          terminarPedido.onclick = (e) => {
             e.preventDefault()
             terminarCompra() 
 
             
+
+            
          }
-        
+         */
 
 
 
